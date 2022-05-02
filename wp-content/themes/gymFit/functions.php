77d387c304c9bce17a5c0e9e3cf5c506b50ed678
estef <estefania@ethecenter.com>
2022-05-02 09:27:06 -0400

    galeria-grid

--- wp-content/themes/gymFit/functions.php
+++ wp-content/themes/gymFit/functions.php
@@ -1,20 +1,22 @@
 <?php
 
-/* consultas reutilizables */
-
+/** Consultas reutilizables */
 require get_template_directory() . '/inc/queries.php';
 
-/* cuando el tema es activado */
-function gymFit_setup() {
-/* habilitar imagenes destacadas */
+// Cuando el tema es activado
+function gymfitness_setup() {
+
+    // Habilitar imagenes destacadas
     add_theme_support('post-thumbnails');
+
+    // Agregar imagenes de tamaño personalizado
     add_image_size('square', 350, 350, true);
-    add_image_size('portait', 350, 724, true);
+    add_image_size('portrait', 350, 724, true);
     add_image_size('cajas', 400, 375, true);
+    add_image_size('mediano', 700, 400, true);
     add_image_size('blog', 966, 644, true);
 }
-
-add_action('after_setup_theme', 'gymFit_setup');
+add_action('after_setup_theme', 'gymfitness_setup');
 
 /* funciones mas personalizadas en functions.php */
 /* no soporta modern view controler */
@@ -43,13 +45,21 @@ function gymFit_scripts_styles() {
     wp_enqueue_style('googleFont','https://fonts.googleapis.com/css2?family=Lato:wght@100&family=Open+Sans&family=Staatliches&display=swap', array(), '1.0.0');
     /* hoja de estilo principal archivo style css */
     /* una vez que corran las dependencias va a correr nuestra hoja de estilos */
-    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFont'), '1.0.0');
+   wp_enqueue_style('style', get_stylesheet_uri(), array('normalize', 'googleFont'), '1.0.0');
+   
+   if(is_page('galeria')):
+   wp_enqueue_style('lightboxCSS', get_template_directory_uri() . '/css/lightbox.min.css', array(), '2.11.0');
+   endif;
    /* url de slicknav JS, usa Jquery como dependencia, para cargar en el footer le ponemos true al final.
    Todos los archivos JS tienen que ir en el footer. Wordpress ya tiene jquery integrado*/
    
     wp_enqueue_script('slicknavJS', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array('jquery'), '1.0.0', true);
+    
+    if(is_page('galeria')):
+    wp_enqueue_script('lightboxJS', get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), '1.0.0', true);
+endif;
     /* url de script JS, le paso dependencia jquery y slicknavJS */
-    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery', 'slicknavJS'), '1.0.0', true);
+    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery', 'slicknavJS'), '2.11.0', true);
 }
 
 /* este hook:cargar hojas de estilo en la parte frontal de la web */
